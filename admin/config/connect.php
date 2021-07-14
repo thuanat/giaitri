1051109df6381a9def500b74de06fc5544f9a8f8
Le Van Thuan <thuanlv2@fpt.edu.vn>
2021-07-14 14:09:48 +0700

    modified connect.php

--- admin/config/connect.php
+++ admin/config/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('52.6.114.59','diem','diem1234','diem');
+$conn = mysqli_connect('52.6.114.59','db-user','diem1234','diemtoy');
 if(!$conn){
 	echo"ket noi that bai";
 }else{
